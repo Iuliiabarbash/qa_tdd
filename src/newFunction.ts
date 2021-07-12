c7551902546f052198fdfa5625f5d9748c2d51ad
Yulia <iuliiabarbash@gmail.com>
2021-07-12 17:38:58 -0400

    solution for the first lab

--- src/newFunction.ts
+++ src/newFunction.ts
@@ -1,1 +1,12 @@
-export function myFunction() {}
+export function myFunction(myValue: number): string {
+    if (myValue == 5) {
+        return "true";
+    } else if (myValue <5 && myValue >= 0) {
+        return "false";
+    } else if (myValue > 5) {
+        return "big";
+    } else {
+        return "negative";
+    }
+}
+console.log(myFunction(10))
