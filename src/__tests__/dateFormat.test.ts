23f25e664137062a67a88cfaaea584db37727556
Yulia <iuliiabarbash@gmail.com>
2021-07-16 16:37:46 -0400

    new update

--- src/__tests__/dateFormat.test.ts
+++ src/__tests__/dateFormat.test.ts
@@ -1,22 +1,22 @@
-function dateFormat(shortDate: string): string {
+let date = "1-1-1"
+
+function dateFormat (shortDate: any): any {
     // this function will accept a numeric short date:
     // MM-DD-YYYY
     // and will return a long date, for example:
     // "2-5-2019" => "February 5, 2019"
     // "11-12-2022" => "November 12, 2022"
-    return "";
+    return `${date}`;
   }
 
-    describe("dateFormat". () => {
-    it("should format shorter short dates correctly": any, () => {
+    describe("dateFormat", () => {
+    it("should format shorter short dates correctly", () => {
         expect(dateFormat("1-1-2000")).toBe("January 1, 2000");
-        expect(dateFormat("3-3-1900")).toBe("March 3, 1900");
     });
-    it("should format longer 'short' date correctly": any, () => {
+    it("should format longer 'short' date correctly", () => {
         expect(dateFormat("01-01-2000")).toBe("January 1, 2000");
-        expect(dateFormat("12-10-2000").toBe("December 10, 2000"));
     });
-    it("should format long past or future dates": any, () => {
+    it("should format long past or future dates", () => {
         expect(dateFormat("1-1-1")).toBe("January 1, 1");
         expect(dateFormat("12-31-99999")).toBe("December 31, 99999")
     });
--- src/newFunction.ts
+++ src/newFunction.ts
@@ -1,12 +1,44 @@
 export function myFunction(myValue: number): string {
-    if (myValue == 5) {
+    if (myValue == 5)
         return "true";
-    } else if (myValue <5 && myValue >= 0) {
+    else if (myValue <5 && myValue >= 0)
         return "false";
-    } else if (myValue > 5) {
+    else if (myValue > 5)
         return "big";
-    } else {
+    else
         return "negative";
-    }
+    
 }
-console.log(myFunction(10))
+console.log(myFunction(100))
+
+//anither practice 
+
+export function anotherFunction(number: number): string {
+    if (number == 5) return "true";
+    if (number < 5 && number >= 0) return "false";
+    if (number > 5) return "big";
+    else return "negative"
+}
+
+console.log(anotherFunction(51))
+
+// let aNumber = 5;
+
+// function thirdFunction() {
+//     switch () {
+//         case `${aNumber} = 5`:
+//             console.log("true");
+//             break
+//         case `${aNumber} < 5 && ${aNumber} >= 0`:
+//             console.log("false");
+//             break
+//         case `${aNumber} > 5`:
+//             console.log('big');
+//             break
+//         default:
+//             console.log('negative');
+//     }
+// }
+
+// console.log(thirdFunction(aNumber));
+
